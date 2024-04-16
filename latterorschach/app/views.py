00d56d8dbe3c8ddfc9e7cc5b8db16e4c00f8ef01
Dominic Miller <98434708+Dominic-Miller@users.noreply.github.com>
2024-04-16 14:03:50 -0400

    Update views.py

--- latterorschach/app/views.py
+++ latterorschach/app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Latte, Interpretation
+from django.contrib import messages
 
 import logging
 
@@ -16,11 +17,27 @@ def today(request):
   return HttpResponse(template.render(context, request))
 
 def register(request):
-    """View function for home page of site."""
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = User.objects.create_user(username=username)
+        user.set_password(password)
+        user.save()
+        return redirect('login')
     return render(request, 'register.html')
 
 def login(request):
-    """View function for home page of site."""
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            return redirect('home')  # Adjust the redirect as necessary
+        else:
+            # Return an 'invalid login' error message.
+            messages.error(request, 'Invalid username or password.')
     return render(request, 'login.html')
 
 def account(request):
